a93db56638a0cad77ea90b1bd628a909769f2e50
Dennis Schröer <Schroeer@adesso-mobile.de>
2023-03-14 17:03:11 +0100

    Add sample page for calculation of 2 numbers

--- BindingLibrarySample/BindingLibrarySample/MainPage.xaml.cs
+++ BindingLibrarySample/BindingLibrarySample/MainPage.xaml.cs
@@ -4,15 +4,36 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BindingLibrarySample.ViewModels;
 using Xamarin.Forms;
 
 namespace BindingLibrarySample
 {
     public partial class MainPage : ContentPage
     {
+        MainPageViewModel viewModel;
+        
         public MainPage()
         {
             InitializeComponent();
+
+            viewModel = (MainPageViewModel)BindingContext;
+        }
+
+        void EntryNumber1_TextChanged(System.Object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
+            if (Int32.TryParse(e.NewTextValue, out int newNumber))
+            {
+                viewModel.Number1 = newNumber;
+            }
+        }
+
+        void EntryNumber2_TextChanged(System.Object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
+            if (Int32.TryParse(e.NewTextValue, out int newNumber))
+            {
+                viewModel.Number2 = newNumber;
+            }
         }
     }
 }
---
+++ BindingLibrarySample/BindingLibrarySample/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,46 @@
+﻿namespace BindingLibrarySample.ViewModels
+{
+    using System;
+    using System.ComponentModel;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public ICommand AddNumbersCommand { get; }
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+
+        private string numbersSumResult = "Start calculating to see the result!";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string NumbersSumResult
+        {
+            get { return numbersSumResult; }
+            set
+            {
+                numbersSumResult = value;
+                OnPropertyChanged(nameof(NumbersSumResult));
+            }
+        }
+
+        public MainPageViewModel()
+        {
+            AddNumbersCommand = new Command(AddNumbers);
+        }
+
+        private void AddNumbers()
+        {
+            var sum = Number1 + Number2;
+            NumbersSumResult = $"Sum: {sum.ToString()}";
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
