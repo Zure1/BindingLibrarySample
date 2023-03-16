32fd7cfc4a85b0835502870045d5d52eda525791
Reimar Twelker <twelker@adesso-mobile.de>
2023-03-16 16:05:03 +0100

    main: Use iOS binding library.

--- BindingLibrarySample/BindingLibrarySample.iOS/Services/AddNumberService.cs
+++ BindingLibrarySample/BindingLibrarySample.iOS/Services/AddNumberService.cs
@@ -1,14 +1,34 @@
 ﻿namespace BindingLibrarySample.iOS.Services
 {
     using System;
+    using Binding;
     using BindingLibrarySample.Services;
 
     public class AddNumberService : IAddNumberService
     {
+#nullable enable
         public int AddNumbers(int number1, int number2)
         {
-            return number1 + number2;
+            Foundation.NSError? error = null;
+
+            var n1 = new PositiveNumber((nint)number1, out error);
+            if (error != null)
+            {
+                throw new ArgumentOutOfRangeException(nameof(number1));
+            }
+
+            var n2 = new PositiveNumber((nint)number2, out error);
+            if (error != null)
+            {
+                throw new ArgumentOutOfRangeException(nameof(number2));
+            }
+
+            var adder = new Adder(); // Adder is the class from the binding library
+            var sum = adder.AddWithA(n1, n2);
+
+            return (int)sum.Value;
         }
+#nullable disable
     }
 }
 
