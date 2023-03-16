21aa002e720b4656a31f71f579625c4237ea0376
Reimar Twelker <twelker@adesso-mobile.de>
2023-03-16 16:04:54 +0100

    main: Add iOS binding library.

---
+++ BindingLibrarySample/BindingLibrarySample.iOS/libs/ComputeThis.resources/ComputeThis.xcframework/ios-arm64_x86_64-simulator/ComputeThis.framework/Headers/ComputeThis.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for ComputeThis.
+FOUNDATION_EXPORT double ComputeThisVersionNumber;
+
+//! Project version string for ComputeThis.
+FOUNDATION_EXPORT const unsigned char ComputeThisVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ComputeThis/PublicHeader.h>
